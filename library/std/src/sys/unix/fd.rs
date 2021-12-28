460545c6cfd086539377189544e1a2c735c8c9d1
Andrea Ciliberti <meziu210@icloud.com>
2021-12-29 00:41:35 +0100

    Fixed network limit

--- library/std/src/sys/unix/fd.rs
+++ library/std/src/sys/unix/fd.rs
@@ -206,7 +206,7 @@ impl FileDesc {
         }
     }
     #[cfg(any(
-        all(target_env = "newlib", not(target_os = "espidf")),
+        all(target_env = "newlib", not(target_os = "espidf"), not(target_os = "horizon")),
         target_os = "solaris",
         target_os = "illumos",
         target_os = "emscripten",
@@ -227,10 +227,10 @@ impl FileDesc {
             Ok(())
         }
     }
-    #[cfg(target_os = "espidf")]
+    #[cfg(any(target_os = "espidf", target_os = "horizon"))]
     pub fn set_cloexec(&self) -> io::Result<()> {
-        // FD_CLOEXEC is not supported in ESP-IDF but there's no need to,
-        // because ESP-IDF does not support spawning processes either.
+        // FD_CLOEXEC is not supported in ESP-IDF and Horizon OS but there's no need to,
+        // because both do not support spawning processes either.
         Ok(())
     }
 
