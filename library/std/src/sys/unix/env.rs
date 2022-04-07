b764bd6cbacbb5d593ad257d4d3aba234ea8d959
Andrea Ciliberti <meziu210@icloud.com>
2022-04-07 08:22:35 +0200

    EXE_SUFFIX value fix

--- library/std/src/sys/unix/env.rs
+++ library/std/src/sys/unix/env.rs
@@ -126,7 +126,7 @@ pub mod os {
     pub const DLL_PREFIX: &str = "lib";
     pub const DLL_SUFFIX: &str = ".so";
     pub const DLL_EXTENSION: &str = "so";
-    pub const EXE_SUFFIX: &str = "";
+    pub const EXE_SUFFIX: &str = ".elf";
     pub const EXE_EXTENSION: &str = "elf";
 }
 
